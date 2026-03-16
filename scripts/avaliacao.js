f0f7e2b651dbf3b8ba5465fa8d2e985e796f4728
Guilherme BR <guilhermedamaia64@gmail.com>
2026-03-16 20:47:27 -0300

    fim da avaliação

---
+++ scripts/avaliacao.js
@@ -0,0 +1,72 @@
+
+
+// função 
+
+//--------------------exercicio1----------------//
+
+function exemplo1() {
+   const numero = Number(prompt("digite um número:"))
+   const numero2 = Number(prompt("digite outro número:")) 
+alert(numero+numero2)
+}
+
+const buttonexemplo1 = document.getElementById("exercicio1")
+buttonexemplo1.addEventListener('click', () => { exemplo1() })// nome da função 
+
+
+
+//--------------------exercicio2---------------------------//
+function exemplo2() {
+      const numero = Number(prompt("digite um número:"))
+   const numero2 = Number(prompt("digite outro número:")) 
+alert(numero*numero2)
+}
+
+const buttonexemplo2 = document.getElementById("exercicio2")
+buttonexemplo2.addEventListener('click', () => { exemplo2() })// nome da função 
+
+
+
+
+
+
+
+
+
+
+//getElementbyid
+// addEventlistner
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
